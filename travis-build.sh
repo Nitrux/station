3f94d27d63bca3513c2d25413567cba80a576acc
UriHerrera <hello@uriherrera.com>
2022-01-17 15:22:17 -0600

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -95,7 +95,7 @@ checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
 	--pkgname=station-git \
-	--pkgversion=2.1.1+git \
+	--pkgversion=2.1.1+git+1 \
 	--pkgarch=amd64 \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
@@ -104,7 +104,7 @@ checkinstall -D -y \
 	--pakdir=../.. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=station \
-	--requires="libc6,libgcc-s1,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 2.1.1+git\),mauikit-filebrowsing-git \(\>= 2.1.1+git\),qml-module-qmltermwidget" \
+	--requires="libc6,libgcc-s1,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 2.1.1+git+1\),mauikit-filebrowsing-git \(\>= 2.1.1+git+1\),qml-module-qmltermwidget" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
